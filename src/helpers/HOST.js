4876491eab005642734cf3dd518e8045bdd1e72f
adln <adlen025@gmail.com>
2021-02-10 18:16:45 +0100

    added data server and language packages

---
+++ src/helpers/HOST.js
@@ -0,0 +1,4 @@
+
+const HOST = { URL: 'http://localhost:8000' }
+
+export default HOST;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+// Load Local Variables
+import HOST from './helpers/HOST';
+// Language
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import frenchMessages from 'ra-language-french';
+
+// Start code
+const i18nProvider = polyglotI18nProvider(() => frenchMessages, 'fr');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Admin i18nProvider={i18nProvider} dataProvider={jsonServerProvider(HOST.URL)}>
+		<Resource name="users" list={ListGuesser}/>
+		<Resource name="agents" list={ListGuesser}/>
+	</Admin>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
