75878bfa2013a5a002c4b409af8535cf26d97f3f
adln <adlen025@gmail.com>
2021-02-28 15:08:45 +0100

    Added search by action, description Added filters by urgent

--- src/Views/Evenements/List.js
+++ src/Views/Evenements/List.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { List, Datagrid, TextField, DateField } from 'react-admin';
+import {
+	List,
+	Datagrid,
+	TextField,
+	DateField,
+	Filter,
+	TextInput,
+	BooleanInput,
+	FunctionField
+} from 'react-admin';
+import Chip from '@material-ui/core/Chip';
 export const EvenementList = (props) => (
-	<List {...props} >
+	<List
+		{...props}
+		filters={
+			<Filter {...props}>
+				<TextInput label="Recherche" source="q" alwaysOn />
+				<BooleanInput label="Urgent" source="urgent" alwaysOn />
+			</Filter>
+		}
+	>
 		<Datagrid rowClick="show">
-      <DateField source="createdAt" showTime locales="fr-FR"/>
+			<FunctionField
+				render={(record) => {
+					return record.urgent ? (
+						<Chip
+							label="Urgent"
+							color="secondary"
+							style={{backgroundColor: 'red'}}
+						/>
+					) : (
+						'non urgent'
+					);
+				}}
+			/>
+			<DateField source="createdAt" showTime locales="fr-FR" />
 			<TextField source="type" />
 			<TextField source="action" />
 			<TextField source="description" />
-
 		</Datagrid>
 	</List>
 );
