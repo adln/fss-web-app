d1ba5d6a6954478777fa5a63455cf398c3e2b172
adln <adlen025@gmail.com>
2021-02-28 15:08:12 +0100

    Added Search by nom, prenom and telephone1 and telephone2

--- src/Views/Agents/List.js
+++ src/Views/Agents/List.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { List, Datagrid, TextField, FunctionField } from 'react-admin';
-export const AgentList = props => (
-	<List {...props}>
-			<Datagrid rowClick="edit">
-					<FunctionField render={(record)=> record.nom + ' ' + record.prenom} label="Nom complet" />
-					<TextField source="telephone1" />
-					<TextField source="adresse" />
-					
-			</Datagrid>
+import { List, Datagrid, TextField, FunctionField, Filter, TextInput } from 'react-admin';
+export const AgentList = (props) => (
+	<List
+		{...props}
+		filters={
+			<Filter {...props}>
+				<TextInput label="Recherche" source="q" alwaysOn />
+			</Filter>
+		}
+	>
+		<Datagrid rowClick="edit">
+			<FunctionField render={(record) => record.nom + ' ' + record.prenom} label="Nom complet" />
+			<TextField source="telephone1" />
+			<TextField source="adresse" />
+		</Datagrid>
 	</List>
 );
